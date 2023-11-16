0d6ed51f8c1752c66370756bd6d77a77b2b64fcd
Eric Ekström <ericekstrm@gmail.com>
2023-11-16 12:22:44 +0100

    added nlohmann json.

---
+++ test/json.test.cc
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include <cpputil.h>
+#include <json.h>
+
+using json = nlohmann::json;
+
+TEST(json, basics)
+{
+    json j = {
+        {"pi", 3.14},
+        {"happy", true}
+    };
+
+    ASSERT_EQ(j["pi"], 3.14);
+    ASSERT_EQ(j["happy"], true);
+
+    j["insert"] = "a string";
+    ASSERT_EQ(j["insert"], "a string");
+}
